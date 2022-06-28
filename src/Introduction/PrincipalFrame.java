a86367c07fa6a959a9cab7dd066091276baebf68
Humberto POSADAS <Posadas.Humberto@outlook.com>
2022-06-28 17:34:59 +0200

    Implementation algoritme logique

--- src/Introduction/PrincipalFrame.java
+++ src/Introduction/PrincipalFrame.java
@@ -30,6 +30,7 @@ public class PrincipalFrame extends javax.swing.JFrame {
         inputValue = new javax.swing.JTextField();
         validateButton = new javax.swing.JButton();
         resultatLabel = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -42,29 +43,39 @@ public class PrincipalFrame extends javax.swing.JFrame {
             }
         });
 
+        jLabel1.setText("Saisir un nombre entre 1 et 100 :");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(37, 37, 37)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(inputValue, javax.swing.GroupLayout.PREFERRED_SIZE, 310, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(resultatLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 257, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(inputValue, javax.swing.GroupLayout.PREFERRED_SIZE, 310, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(31, 31, 31)
                 .addComponent(validateButton, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 49, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(resultatLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addComponent(jLabel1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(64, 64, 64)
+                .addGap(15, 15, 15)
+                .addComponent(jLabel1)
+                .addGap(33, 33, 33)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(inputValue, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(validateButton, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 41, Short.MAX_VALUE)
-                .addComponent(resultatLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(78, 78, 78))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 70, Short.MAX_VALUE)
+                .addComponent(resultatLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(62, 62, 62))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -87,16 +98,30 @@ public class PrincipalFrame extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    int valeur = (int)(Math.random()*100);
+    int live = 0;
     private void validateButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_validateButtonActionPerformed
-        int valeur = (int)(Math.random()*100);
         //saisir la valeur au clavier
         int userValue = Integer.parseInt(inputValue.getText());
-        if(valeur == userValue) {
-            //cas gagnant
-            resultatLabel.setText("Felicitations vous avez trouvé la réponse !!!");
-        } else {
-            resultatLabel.setText("Désolé mais vous avez raté");
+
+        if(userValue < valeur) {
+            resultatLabel.setText("Votre valeure est inferieure.");
+            if(live == 4){
+                resultatLabel.setText("Perdu, vous n'avez plus de tentatives. La valeur cherchée etait " + valeur);
+                validateButton.setEnabled(false);
+            }
+        } else if(userValue > valeur) {
+            resultatLabel.setText("Votre valeure est superieure.");
+            if(live == 4){
+                resultatLabel.setText("Perdu, vous n'avez plus de tentatives. La valeur cherchée etait " + valeur);
+                validateButton.setEnabled(false);
+            }
+        } else if(userValue == valeur) {
+            resultatLabel.setText("Felicitations, vous avez trouvé la valeure");
+            validateButton.setEnabled(false);
         }
+        
+        live++;
     }//GEN-LAST:event_validateButtonActionPerformed
 
     /**
@@ -136,6 +161,7 @@ public class PrincipalFrame extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField inputValue;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel resultatLabel;
     private javax.swing.JButton validateButton;
